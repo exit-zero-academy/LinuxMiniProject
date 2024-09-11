24ed1dbb3104064673d83ae3fc689c3a91325f37
Vansh Gupta <131511941+Vansh16GUPTA@users.noreply.github.com>
2024-09-11 14:18:52 +0530

    Update mySolution.sh

---
+++ mySolution.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+wget https://exit-zero-academy.github.io/DevOpsTheHardWayAssets/linux_project/secretGenerator.tar.gz
+
+gunzip secretGenerator.tar.gz
+
+tar -xvf secretGenerator.tar
+
+cd ./src
+
+mkdir secretDir
+cd ./maliciousFiles
+
+
+rm amIMaliciousOrNot.whoKnows someFileIsLinkingToMe.BeAware
+
+cd ..
+ 
+rmdir maliciousFiles
+
+
+
+mv generateSecret.sh ./secretDir/
+
+cd ./secretDir/
+
+touch .secret
+
+chmod 600 .secret
+
+chmod 777 generateSecret.sh
+
+cd ..
+
+./secretDir/generateSecret.sh
