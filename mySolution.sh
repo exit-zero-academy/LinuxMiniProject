e8febe7ea636dcb11c65ce1eee259863db3db7de
Dhruv-Jain31 <126942955+Dhruv-Jain31@users.noreply.github.com>
2024-09-14 00:16:14 +0530

    added commands to mySolution.sh and added secret to SOLUTION

---
+++ mySolution.sh
@@ -0,0 +1,9 @@
+wget https://exit-zero-academy.github.io/DevOpsTheHardWayAssets/linux_project/secretGenerator.tar.gz
+tar -xvzf secretGenerator.tar.gz
+cd src
+mkdir secretDir
+rm -r maliciousFiles
+touch secretDir/.secret
+chmod 600 secretDir/.secret
+chmod +x generateSecret.sh
+./generateSecret.sh
