376d96b7fe8246373ddc49fb17801f5fbcb40043
Anurag Negi <an6265081@gmail.com>
2024-09-14 21:27:14 +0530

    fixed

--- mySolution.sh
+++ mySolution.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-wget https://exit-zero-academy.github.io/DevOpsTheHardWayAssets/linux_project/secretGenerator.tar.gz
-tar -xzvf secretGenerator.tar.gz
 cd src
 rm -rf maliciousFiles
 mkdir -p secretDir
