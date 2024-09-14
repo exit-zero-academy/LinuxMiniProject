8f82efa020c9058725f19c9427b5de781d9415b2
KhushiJain2004 <kj866795@gmail.com>
2024-09-14 18:46:19 +0530

    update

--- mySolution.sh
+++ mySolution.sh
@@ -1,16 +1,11 @@
-#!/bin/bash
-wget https://exit-zero-academy.github.io/DevOpsTheHardWayAssets/linux_project/secretGenerator.tar.gz
-gunzip secretGenerator.tar.gz
-tar -xvf secretGenerator.tar
 cd src
-rm -rf ./maliciousFiles
-mkdir -p ./secretDir
-touch ./secretDir/.secret
-chmod 600 ./secretDir/.secret
-
-chmod +x ./generateSecret.sh
-
+rm -rf maliciousFiles/
+mkdir -p secretDir
+touch secretDir/.secret
+chmod 600 secretDir/.secret
+chmod +x generateSecret.sh
 ./generateSecret.sh
-SECRET=$(head -n 1 ./secretDir/.secret)
+
+SECRET=$(head -n 1 secretDir/.secret)
 cd ..
 echo "$SECRET" | cut -c -32 > SOLUTION
