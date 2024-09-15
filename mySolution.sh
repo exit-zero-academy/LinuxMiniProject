0e6623fd1b2a4fc30bc68874677b4bd6c8e03145
ughrima <agrimajain223@gmail.com>
2024-09-15 23:31:01 +0530

    Fixed path issues and ensured secretDir exists

--- mySolution.sh
+++ mySolution.sh
@@ -1,22 +1,9 @@
-#!/bin/bash
 
-# Create secretDir if it doesn't exist
-mkdir -p secretDir
-
-# Create the .secret file
-touch secretDir/.secret
-
-# Set permissions for the .secret file
-chmod 600 secretDir/.secret
-
-# Remove malicious files directory if it exists
-if [ -d "maliciousFiles" ]; then
-  rm -r maliciousFiles
-fi
-
-# Run the script to generate the secret
-cd src
+cd src/ || exit
+mkdir secretDir
+rm -rf maliciousFiles
+cd secretDir || exit
+touch .secret
+chmod 600 .secret
+cd ../
 /bin/bash generateSecret.sh
-
-# Indicate that the process is complete
-echo "Done! Your secret was stored in secretDir/.secret"
