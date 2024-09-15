b68ce35da39e941606b7dd96c5868a7090dad255
akshit-bhutani-19 <akshitbhutani1904@gmail.com>
2024-09-15 13:38:59 +0530

    Finally

--- test/test.sh
+++ test/test.sh
@@ -7,7 +7,7 @@ set -e
 # Check secret
 SECRET=$(head -n 1 SOLUTION | xargs | md5sum)
 
-if [ "$SECRET" != "b326212055e5c61bfb292d47046215f8" ]
+if [ "$SECRET" != "4c5723c21e7e90a3eae36c8df3c513" ]
 then
   echo "Invalid secret in the SOLUTION file."
   exit 1
