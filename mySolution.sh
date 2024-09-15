6f65b68131c50077775683456907b000baa3317b
Dhruv Chaubey <143328600+dhruvchaubey28@users.noreply.github.com>
2024-09-15 19:26:57 +0530

    Update mySolution.sh

--- mySolution.sh
+++ mySolution.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
+
 # Navigate to src directory
-cd src
+cd src || exit
 
 # Remove the maliciousFiles directory
 rm -rf maliciousFiles
@@ -8,7 +10,7 @@ rm -rf maliciousFiles
 mkdir -p secretDir
 
 # Navigate into secretDir
-cd secretDir
+cd secretDir || exit
 
 # Create a .secret file
 touch .secret
