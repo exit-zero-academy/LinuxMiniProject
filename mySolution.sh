d7d4663207dc5c66a284daebe37ee20d77d643a2
MOHIT KUMAR <bluesky5april2021@gmail.com>
2024-09-15 15:37:38 +0530

    Added the bash script in mySolution.sh

---
+++ mySolution.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+tar -xvf secretGenerator.tar.gz
+cd src
+
+mkdir secretDir
+rm -rf maliciousFiles
+touch secretDir/.secret
+chmod 600 secretDir/.secret
+/bin/bash generateSecret.sh
+cd secretDir
+cat .secret
