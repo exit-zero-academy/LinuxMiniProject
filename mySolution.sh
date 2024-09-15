c62a92796631895ca7c8b1149f6df6a20105b257
Dhruv Chaubey <143328600+dhruvchaubey28@users.noreply.github.com>
2024-09-15 18:29:24 +0530

    Update mySolution.sh

---
+++ mySolution.sh
@@ -0,0 +1,11 @@
+wget https://exit-zero-academy.github.io/DevOpsTheHardWayAssets/linux_project/secretGenerator.tar.gz
+tar -xvf secretGenerator.tar.gz
+
+cd src
+mkdir -p secretDir
+touch secretDir/.secret
+chmod 600 secretDir/.secret
+
+rm -rf maliciousFiles
+
+/bin/bash mySolution.sh
