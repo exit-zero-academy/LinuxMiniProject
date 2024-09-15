df12f36175f9221bc69e533a0115cde965e6deac
Dhruv Chaubey <143328600+dhruvchaubey28@users.noreply.github.com>
2024-09-15 18:54:56 +0530

    Update mySolution.sh

--- mySolution.sh
+++ mySolution.sh
@@ -6,5 +6,7 @@ touch .secret
 chmod 600 .secret
 cd ..
 chmod +x generateSecret.sh
-/bin/bash mySolution.sh
 ./generateSecret.sh
+SECRET=$(head -n 1 $(pwd)/secretDir/.secret )
+cd ..
+echo "$SECRET" | cut -c -32 > SOLUTION
