7c1134c548a4bce41054e0645b625527a213b7f5
Dhruv Chaubey <143328600+dhruvchaubey28@users.noreply.github.com>
2024-09-15 19:01:51 +0530

    Update mySolution.sh

--- mySolution.sh
+++ mySolution.sh
@@ -1,3 +1,13 @@
+# cd src
+# rm -rf $(pwd)/maliciousFiles/
+# echo $(pwd)
+# mkdir -p $(pwd)/secretDir
+# touch $(pwd)/secretDir/.secret
+# chmod 600 $(pwd)/secretDir/.secret
+
+
+# chmod +x $(pwd)/generateSecret.sh
+# ./generateSecret.sh
 cd src
 rm -rf maliciousFiles
 mkdir secretDir
@@ -7,6 +17,6 @@ chmod 600 .secret
 cd ..
 chmod +x generateSecret.sh
 ./generateSecret.sh
-SECRET=$(head -n 1 $(pwd)/secretDir/.secret )
+SECRET=$(head -n 1 $(pwd)/secretDir/.secret)
 cd ..
 echo "$SECRET" | cut -c -32 > SOLUTION
