3e0b0685c38d299b6c3cabc7a7e78c3f179269b8
Saarthakgupta26 <143247640+Saarthakgupta26@users.noreply.github.com>
2024-09-15 18:20:31 +0530

    Update mySolution.sh

---
+++ mySolution.sh
@@ -0,0 +1,11 @@
+gunzip secretGenerator.tar.gz
+tar -xvf secretGenerator.tar
+cd src
+rm -rf maliciousFiles
+mkdir secretDir
+cd secretDir
+touch .secret
+chmod 600 .secret
+cd ..
+chmod +x generateSecret.sh
+./generateSecret.sh
