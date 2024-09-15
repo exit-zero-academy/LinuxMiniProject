4cc8be2caafa6a093d30f9d5056f34b7784bf046
addittidas <aditidas.coding@gmail.com>
2024-09-15 19:10:27 +0530

    linux project - final solution

--- mySolution.sh
+++ mySolution.sh
@@ -1,3 +1,11 @@
 wget https://exit-zero-academy.github.io/DevOpsTheHardWayAssets/linux_project/secretGenerator.tar.gz
-tar -xvf secretGenerator.tar.gz
-/bin/bash mySolution.sh
+gunzip secretGenerator.tar.gz
+tar -xvf secretGenerator.tar
+cd src/ || exit
+mkdir secretDir
+rm -rf maliciousFiles
+cd secretDir || exit
+touch .secret
+chmod 600 .secret
+cd ../
+/bin/bash generateSecret.sh
