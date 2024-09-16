3c7df6bbc8227b9f00df86fa506eaed90489e9a3
Dhruv Chaubey <143328600+dhruvchaubey28@users.noreply.github.com>
2024-09-16 10:57:06 +0530

    Update mySolution.sh

--- mySolution.sh
+++ mySolution.sh
@@ -1,34 +1,12 @@
-#!/bin/bash
-
-# Navigate to src directory
-cd src || exit
-
-# Remove the maliciousFiles directory
+cd src
 rm -rf maliciousFiles
-
-# Create the secretDir directory
-mkdir -p secretDir
-
-# Navigate into secretDir
-cd secretDir || exit
-
-# Create a .secret file
+mkdir secretDir
+cd secretDir
 touch .secret
-
-# Set permissions to read/write for the owner only
 chmod 600 .secret
-
-# Navigate back to the parent directory
 cd ..
-
-# Make the generateSecret.sh script executable
 chmod +x generateSecret.sh
-
-# Execute the generateSecret.sh script
 ./generateSecret.sh
-
-# Extract the first line of the .secret file
-SECRET=$(head -n 1 secretDir/.secret)
-
-# Save the first 32 characters of SECRET to SOLUTION
+SECRET=$(head -n 1 $(pwd)/secretDir/.secret)
+cd ..
 echo "$SECRET" | cut -c -32 > SOLUTION
